da963ec305c8ca3ead5006b65165a876a7c07567
Muhammad Shahalshad <shahallshad@gmail.com>
2024-12-02 11:45:15 +0530

    4 completed

--- 02-dec-assesment-text/test.py
+++ 02-dec-assesment-text/test.py
@@ -9,3 +9,18 @@ for num in range(0,11,2):
 
 dividedby3 = [num for num in range(1,51)if num % 3 ==0]
 print(dividedby3)
+
+st = 'Go through the string below and if the length of a word is even print "even!"'
+
+# Split the string into words and iterate through them
+for word in st.split():
+    # Check if the length of the word is even
+    if len(word) % 2 == 0:
+        print(f'{word} - even!')
+
+
+myWords = 'Print every word in this sentence that has an even number of letters'
+for wordin in myWords.split():
+    if len(wordin) %2==0:
+        print(f'{wordin} - even!*!')
+

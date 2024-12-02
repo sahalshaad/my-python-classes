a0dd2028bfdad8ae10900016bb2017ec700d5481
Muhammad Shahalshad <shahallshad@gmail.com>
2024-12-02 12:03:12 +0530

    commit

--- 02-dec-assesment-text/test.py
+++ 02-dec-assesment-text/test.py
@@ -23,4 +23,30 @@ myWords = 'Print every word in this sentence that has an even number of letters'
 for wordin in myWords.split():
     if len(wordin) %2==0:
         print(f'{wordin} - even!*!')
+# Write a program that prints the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number, and for the multiples of
+# five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 
+for num in range(1, 101):
+    # Check for multiples of both 3 and 5
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    # Check for multiples of 3
+    elif num % 3 == 0:
+        print("Fizz")
+    # Check for multiples of 5
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+
+
+for numb in range(1,101):
+    # print(numb)
+    if numb % 3 ==0 and numb % 5 ==0:
+        print('fiz3buzz5')
+    elif numb % 3==0:
+        print('buzz')
+    elif numb % 5 ==0:
+        print('fizz')
+    else:
+        print(numb)
